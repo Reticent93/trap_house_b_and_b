a9b811d5420c4c1f5c23c0f41c8647c1adf32c99
gren8 <greg.renfro@gmail.com>
2021-07-23 17:21:53 -0700

    added datepicker

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,33 +16,34 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
+
 	// change to true when in production
 	app.InProduction = false
-	
-	session := scs.New()
+
+	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
 	session.Cookie.SameSite = http.SameSiteLaxMode
 	session.Cookie.Secure = app.InProduction
-	
+
 	app.Session = session
-	
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	app.TemplateCache = tc
 	app.UseCache = false
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-	
+
 	render.NewTemplates(&app)
-	
+
 	fmt.Println(fmt.Sprintf("Starting on port %s", port))
-	
+
 	srv := &http.Server{Addr: port, Handler: routes(&app)}
 	err = srv.ListenAndServe()
 	log.Fatal(err)
-	
+
 }
